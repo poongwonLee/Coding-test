d204a1ceb60d2cfad340ad4138374e3e97d9d3d1
PungwonLee <64236372+PungwonLee@users.noreply.github.com>
2021-10-17 16:43:38 +0900

    상근이의 여행

---
+++ baekjoon/9372(s5).py
@@ -0,0 +1,11 @@
+import sys
+from collections import deque
+si = sys.stdin.readline
+
+k=int(si())
+while(k):
+    k-=1
+    n,m=map(int,si().split())
+    for i in range(m):
+        a,b=map(int,si().split())
+    print(n-1)
