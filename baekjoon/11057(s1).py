81856c1a956b76dfa489ab918dd1860d98fd366d
PungwonLee <64236372+PungwonLee@users.noreply.github.com>
2021-12-08 23:58:28 +0900

    오르막 수 (DP,다이나믹프로그래밍)

--- baekjoon/11057(s1).py
+++ baekjoon/11057(s1).py
@@ -8,10 +8,8 @@ for i in range(10):
 
 n=int(input())
 for i in range(2,n+1):
-  
-  for j in range(10):
-    for k in range(j+1):
-      dp[i][j]+=dp[i-1][k]
-      dp[i][j]%=10007
+  dp[i][0]=1
+  for j in range(1,10):
+    dp[i][j]=(dp[i-1][j]+dp[i][j-1])%10007
 
-print(sum(dp[n])) 
+print(sum(dp[n])%10007) 
