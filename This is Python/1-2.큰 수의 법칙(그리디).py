63dfc58b825bceeea51e109623bd79e45fcce616
PungwonLee <64236372+PungwonLee@users.noreply.github.com>
2022-08-08 20:00:52 +0900

    Delete This is Python directory

--- This is Python/1-2.큰 수의 법칙(그리디).py
+++
@@ -1,9 +0,0 @@
-n,m,k=map(int,input().split())
-arr=list(map(int,input().split()))
-arr.sort(reverse=True) #큰 순으로 정렬
-
-if arr[0]==arr[1]:     #가장 큰 값이 2개 일때
-    print(arr[0]*m)
-else:
-    cnt=m//(k+1)       #k+1 수열로 반복 하기 때문에 m에 k+1를 나눈다
-    print((m-cnt)*arr[0]+cnt*arr[1])
--- This is Python/1-3.숫자 카드 게임(그리디).py
+++
@@ -1,8 +0,0 @@
-n,m=map(int,input().split()) # n by m 행렬 입력
-c=[]
-max1=0          # 최대값 저장할 변수 0으로 초기화
-for i in range(n):  #n행만큼 반복
-    c.append(list(map(int,input().split())))    #n by m 행렬 입력
-    min1=min(c[i])                              # c[i] i행 원소들의 최소값을 min1 에 저장
-    max1=max(max1,min1)                         #max1에 각행의 최소값들과 비교해 최대값 저장
-print(max1)                                     #max1 출력 결과.
