29d7081b6990d11319668da2b275e00e7d086c1e
PungwonLee <64236372+PungwonLee@users.noreply.github.com>
2022-08-09 14:09:51 +0900

    [D4] Title: [S/W 문제해결 기본] 9일차 - 사칙연산 유효성 검사, Time: 134 ms, Memory: 21,484 KB -BaekjoonHub

---
+++ SWEA/D4/1233. ［S／W 문제해결 기본］ 9일차 － 사칙연산 유효성 검사/［S／W 문제해결 기본］ 9일차 － 사칙연산 유효성 검사.java
@@ -0,0 +1,56 @@
+
+import java.io.*;
+import java.util.*;
+
+
+class Solution {
+	
+	public static String []arr;
+	public static boolean flag;
+	public static void dfs(int v,int n) {
+		if (v*2+1>n)return;
+		dfs(v*2,n);
+		dfs(v*2+1,n);
+		
+		if(v/2==0) return;
+		if(!Character.isDigit(arr[v*2].charAt(0)) && !Character.isDigit(arr[v*2+1].charAt(0))) flag=false;
+		arr[v]="1";
+
+	}
+	public static void main(String args[]) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+
+		int testCase = 10;
+		for (int t = 1; t <= testCase; t++) {
+			flag=true;
+			st = new StringTokenizer(br.readLine());
+			int n = Integer.parseInt(st.nextToken());
+			
+			arr= new String[n+1];
+			for (int i = 1; i <= n; i++) {
+				String []str= br.readLine().split(" ");
+				arr[i]=str[1];
+			}
+			dfs(1,n);
+			sb.append("#").append(t).append(" ");
+			sb.append(flag ? 1:0);
+			sb.append("\n");
+		}
+		System.out.println(sb);
+	}
+}
+
+/*
+9
+1 * 2 3
+2 + 4 5
+3 - 6 7
+4 / 8 9
+5 2
+6 6
+7 4
+8 8
+9 7
+*/
