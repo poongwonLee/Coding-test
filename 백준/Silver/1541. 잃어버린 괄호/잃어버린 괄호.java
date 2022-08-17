1df4ae3c596066463107b44124df9a3b1848e585
PungwonLee <64236372+PungwonLee@users.noreply.github.com>
2022-08-17 15:28:18 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 128 ms, Memory: 14252 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
@@ -1,4 +1,3 @@
-import java.awt.Point;
 import java.io.*;
 import java.util.*;
 
@@ -9,33 +8,26 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 		String str = br.readLine();
-		int[] arr = Arrays.stream(str.split("\\+|-")).mapToInt(Integer::parseInt).toArray();
-		int[] ch= new int[arr.length];
-		
-		int idx = 0;
-		int idx2=arr.length-1;
-		for (int i = 0; i < str.length(); i++) {
-			if(str.charAt(i)=='+') {
-				arr[idx+1]+=arr[idx];
-				ch[idx]=1;
-				idx++;
-			}
-			else if (str.charAt(i)=='-') {
-				idx++;
-			}	
-		}
-		int res=0;
-		boolean flag=true;
-		for (int i = 0; i < ch.length; i++) {
-			if(ch[i]==0 && flag) {
-				res=arr[i];
-				flag=false;
-			}
-			else if(ch[i]==0) {
-				res-=arr[i];
-			}
-		}
-		System.out.println(res);
+		String[] strList = str.split("-");
 
+		ArrayList<Integer> arr = new ArrayList<>();
+		for (int i = 0; i < strList.length; i++) {
+			int addNum = 0;
+			if (strList[i].contains("+")) {
+				
+				String[] tmpList = strList[i].split("\\+");
+				for (int j = 0; j < tmpList.length; j++)
+					addNum += Integer.parseInt(tmpList[j]);
+				
+			} else
+				addNum = Integer.parseInt(strList[i]);
+			arr.add(addNum);
+		}
+		
+		
+		int res=arr.get(0);
+		for (int i = 1; i < arr.size(); i++)
+			res-=arr.get(i);
+		System.out.println(res);
 	}
 }
