5c8daf5625ff8c2c775ba38a9c03a6bb8b3c79d2
PungwonLee <64236372+PungwonLee@users.noreply.github.com>
2022-09-29 18:11:13 +0900

    [Silver I] Title: RGB거리, Time: 292 ms, Memory: 21868 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.java
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -1,26 +1,15 @@
 import java.util.*;
-
 class Main {
-
-	public static void main(String args[]) throws Exception {
+	public static void main(String args[]) {
 		Scanner sc = new Scanner(System.in);
 		int n= sc.nextInt();
-		int [][]arr=new int[3][3];
-
-		
+		int [][]arr=new int[2][3];
 		for(int i=0;i<n;i++) {
-			for(int j=0;j<3;j++) 
-				arr[1][j]=sc.nextInt();
-			arr[0][0]=Math.min(arr[2][1], arr[2][2]);
-			arr[0][1]=Math.min(arr[2][0], arr[2][2]);
-			arr[0][2]=Math.min(arr[2][0], arr[2][1]);
-			
-			for(int j=0;j<3;j++) 
-				arr[2][j]=arr[1][j]+arr[0][j];
-			
+			arr[0][0]=Math.min(arr[1][1], arr[1][2]);
+			arr[0][1]=Math.min(arr[1][0], arr[1][2]);
+			arr[0][2]=Math.min(arr[1][0], arr[1][1]);
+			for(int j=0;j<3;j++) arr[1][j]=arr[0][j]+sc.nextInt();
 		}
-		System.out.println(Arrays.stream(arr[2]).min().getAsInt());
-	
-		
+		System.out.println(Arrays.stream(arr[1]).min().getAsInt());
 	}
 }
